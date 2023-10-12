3974bcfb0ddd419b674942c1168401fd97bf4de7
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-12 19:28:25 +0200

    add Participation model

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -1,11 +1,22 @@
-// TODO: create here a typescript interface for a participation
-/*
-example of participation:
-{
-    id: 1,
-    year: 2012,
-    city: "Londres",
-    medalsCount: 28,
-    athleteCount: 372
+/**
+ * @interface Participation
+ *
+ * @description
+ * A Participation is a single participation of a country in the Olympic Games in a specific year
+ *
+ * @example
+ * const myOlympicParticipation: Participation = {
+ *  id: 1,
+ *  year: 2012,
+ *  city: "Londres",
+ *  medalsCount: 28,
+ *  athleteCount: 372
+ * }
+ */
+export interface Participation {
+  id: number;
+  year: number;
+  city: string;
+  medalsCount: number;
+  athleteCount: number;
 }
-*/
