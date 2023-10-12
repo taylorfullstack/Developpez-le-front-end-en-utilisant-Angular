b2e37caba98432a057ac7f366f532d37ad450fca
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-12 19:28:40 +0200

    add Olympic country model

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,22 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+import { Participation } from "./Participation";
+
+/**
+ * @interface Olympic
+ *
+ * @description
+ * An Olympic is a country that has participated in the Olympic Games
+ *
+ * This interface should be used in the olympic.service.ts
+ *
+ * @example
+ * const olympicCountry: Olympic = {
+ *  id: 1,
+ *  country: "Italy",
+ *  participations: [],
+ * }
+ */
+export interface Olympic {
+  id: number;
+  country: string;
+  participations: Participation[];
 }
-*/
