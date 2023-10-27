12b6c09b2ff8df9eed64ab327d3ac7a5de894acb
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-27 13:19:07 +0200

    update home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component} from '@angular/core';
+import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -7,12 +7,31 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+export class HomeComponent{
+  //public olympics!: Observable<Olympic[]>;
+  //olympics$!: Observable<Olympic[]>;
+
+  olympics$!: Olympic[];
+  labels: string[] = [];
+  medals: number[] = [];
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.getChartData();
+  }
+
+  //TODO - see if this should be moved to the service
+  getChartData(): void {
+    this.olympicService.getOlympics().subscribe(olympics => {
+      this.olympics$ = olympics;
+      console.log("olympics", this.olympics$);
+      this.olympics$.forEach(country => {
+        this.labels.push(country.country);
+        this.medals.push(this.olympicService?.getTotalMedalsByCountryId(country.id));
+      });
+      console.log("labels", this.labels);
+      console.log("medals", this.medals);
+    });
   }
 }
