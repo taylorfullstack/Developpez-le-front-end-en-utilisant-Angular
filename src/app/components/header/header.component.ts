81037fec253216cc44d02b0e5fa7f63d95735440
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-27 21:27:31 +0200

    update header component, remove ngIf and nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
-
-  isNotHomePage(): boolean {
-    return this.router.url !== ' ';
-  }
+  constructor() {}
 }
