14229a860e833ab2a474d6c91e1eedd3639b1441
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-27 13:17:01 +0200

    update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CountryComponent } from './pages/country/country.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: ':id', // country id
+    component: CountryComponent,
+  },
+  {
+    path: '', // default route
     component: HomeComponent,
   },
   {
