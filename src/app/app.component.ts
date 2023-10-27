db9ad6fe5e2b0f7bd4a651968892f15554af0b6e
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-27 13:17:39 +0200

    updatte app and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs';
-import { OlympicService } from './core/services/olympic.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +6,6 @@ import { OlympicService } from './core/services/olympic.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private olympicService: OlympicService) {}
-
-  ngOnInit(): void {
-    this.olympicService.loadInitialData().pipe(take(1)).subscribe();
-  }
+  constructor(/*private olympicService: OlympicService*/) {}
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
 import { HomeComponent } from './pages/home/home.component';
+import { CountryComponent } from './pages/country/country.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CountryPieSliceComponent } from './components/country-pie-slice/country-pie-slice.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StatsCardComponent } from './components/stats-card/stats-card.component';
+import { PiechartComponent } from './components/piechart/piechart.component';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgChartsModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CountryComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    CountryPieSliceComponent,
+    StatsCardComponent,
+    PiechartComponent
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
