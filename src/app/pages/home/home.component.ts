dc7bd428f7408a05d36852d8be0e1d738abe393a
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-27 21:34:30 +0200

    add ActivatedRoute to home page #1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,30 +9,28 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent{
-  //public olympics!: Observable<Olympic[]>;
-  //olympics$!: Observable<Olympic[]>;
-
   olympics$!: Olympic[];
   labels: string[] = [];
   medals: number[] = [];
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     this.getChartData();
+    //console.log("route", this.route);
   }
 
-  //TODO - see if this should be moved to the service
   getChartData(): void {
     this.olympicService.getOlympics().subscribe(olympics => {
       this.olympics$ = olympics;
-      console.log("olympics", this.olympics$);
+      //console.log("olympics", this.olympics$);
       this.olympics$.forEach(country => {
         this.labels.push(country.country);
         this.medals.push(this.olympicService?.getTotalMedalsByCountryId(country.id));
       });
-      console.log("labels", this.labels);
-      console.log("medals", this.medals);
+      //console.log("labels", this.labels);
+      //console.log("medals", this.medals);
     });
   }
 }
