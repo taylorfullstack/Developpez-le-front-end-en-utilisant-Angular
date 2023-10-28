a5e00c0b4a2c0dc5132ca1a73dd25312332bb72a
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-10-28 16:24:07 +0200

    refactor piechart, correct types, add colors

--- src/app/components/piechart/piechart.component.ts
+++ src/app/components/piechart/piechart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';
-import { Chart, ChartEvent} from 'chart.js';
+import { Chart, ChartEvent, TooltipModel} from 'chart.js';
 import { ActiveElement} from 'chart.js/dist/plugins/plugin.tooltip';
 @Component({
   selector: 'app-piechart',
@@ -10,171 +10,180 @@ import { ActiveElement} from 'chart.js/dist/plugins/plugin.tooltip';
 export class PiechartComponent implements OnInit{
     @Input({ required: true }) labels!: string[];
     @Input({ required: true }) medals!: number[];
+    chartElement!: HTMLCanvasElement;
+    chart!: Chart<'pie'>;
+    accessibleColorPalette = ["#A8385D", "#7AA3E5", "#A27EA8", "#A6E9F4", "#ADCDED", "#A95963" ]
 
-    constructor(private router: Router) {}
+    constructor(private router: Router) {
+    }
 
     ngOnInit(): void {
-      const chartElement = document.getElementById('pie-chart') as HTMLCanvasElement;
-      if (chartElement) {
-        this.renderChart(this.labels, this.medals, chartElement);
+      const pieChartCanvas = document.getElementById('pie-chart');
+      if(pieChartCanvas instanceof HTMLCanvasElement) {
+        this.chartElement = pieChartCanvas;
+        this.renderChart();
+        //this.chart.update("resize");
+      };
+    }
+
+    getOrCreateTooltip = (chart: Chart) => {
+      let tooltipEl = chart.canvas?.parentNode?.querySelector('div');
+
+      if (!tooltipEl) {
+        // Create tooltip element
+        tooltipEl = document.createElement('div');
+        tooltipEl.style.background = '#04838f';
+        tooltipEl.style.borderRadius = '8px';
+        tooltipEl.style.color = 'white';
+        tooltipEl.style.opacity = '1';
+        tooltipEl.style.pointerEvents = 'none';
+        tooltipEl.style.position = 'absolute';
+        tooltipEl.style.transform = 'translate(-50%, 0)';
+        tooltipEl.style.transition = 'all .1s ease';
+
+        // Create element for tooltip table
+        const table = document.createElement('table');
+        table.style.margin = '0px';
+        table.style.display = 'flex';
+        table.style.flexDirection = 'column';
+        table.style.alignItems = 'center';
+
+        tooltipEl.appendChild(table);
+        chart.canvas?.parentNode?.appendChild(tooltipEl);
       }
-    }
 
-      renderChart(labels: string[] , medals: number[], chartElement: HTMLCanvasElement ){
+      return tooltipEl;
+    };
 
-        const getOrCreateTooltip = (chart: { canvas: { parentNode: { querySelector: (arg0: string) => HTMLElement | null; appendChild: (arg0: HTMLElement) => void; }; }; }) => {
-          let tooltipEl = chart.canvas.parentNode.querySelector('div');
+    externalTooltipHandler = (context: { chart: Chart; tooltip: TooltipModel<'pie'>; }) => {
+      // Tooltip Element
+      const {chart, tooltip} = context;
+      console.log('context', context)
+      console.log('chart', chart)
+      console.log('tooltip', tooltip)
+      const tooltipEl = this.getOrCreateTooltip(chart);
 
-          if (!tooltipEl) {
-            // Create tooltip element
-            tooltipEl = document.createElement('div');
-            tooltipEl.style.background = '#04838f';
-            tooltipEl.style.borderRadius = '8px';
-            tooltipEl.style.color = 'white';
-            tooltipEl.style.opacity = '1';
-            tooltipEl.style.pointerEvents = 'none';
-            tooltipEl.style.position = 'absolute';
-            tooltipEl.style.transform = 'translate(-50%, 0)';
-            tooltipEl.style.transition = 'all .1s ease';
+      // Hide if no tooltip
+      if (tooltip.opacity === 0) {
+        tooltipEl.style.opacity = '0';
+        return;
+      }
 
-            // Create element for tooltip table
-            const table = document.createElement('table');
-            table.style.margin = '0px';
-            table.style.display = 'flex';
-            table.style.flexDirection = 'column';
-            table.style.alignItems = 'center';
+      // Set Text
+      if (tooltip.body) {
+        const titleLines = tooltip.title || [];
+        const bodyLines = tooltip.body.map((b: { lines: string[]}) => b.lines);
 
-            tooltipEl.appendChild(table);
-            chart.canvas.parentNode.appendChild(tooltipEl);
-          }
+        const tableHead = document.createElement('thead');
 
-          return tooltipEl;
-        };
+        titleLines.forEach((title: string) => {
+          const tr = document.createElement('tr');
+          tr.style.borderWidth = '0';
 
-        const externalTooltipHandler = (context: { chart: any; tooltip: any; }) => {
-          // Tooltip Element
-          const {chart, tooltip} = context;
-          const tooltipEl = getOrCreateTooltip(chart);
+          const th = document.createElement('th');
+          th.style.borderWidth = '0';
+          const text = document.createTextNode(title);
 
-          // Hide if no tooltip
-          if (tooltip.opacity === 0) {
-            tooltipEl.style.opacity = '0';
-            return;
-          }
+          th.appendChild(text);
+          tr.appendChild(th);
+          tableHead.appendChild(tr);
+        });
 
-          // Set Text
-          if (tooltip.body) {
-            const titleLines = tooltip.title || [];
-            const bodyLines = tooltip.body.map((b: { lines: number; }) => b.lines);
+        const tableBody = document.createElement('tbody');
+          bodyLines.forEach((body, _i) => {
+          //const colors = tooltip.labelColors[i];
 
-            const tableHead = document.createElement('thead');
+          const span = document.createElement('span');
+          //span.style.background = colors.backgroundColor;
+          //span.style.borderColor = colors.borderColor;
+          span.style.borderWidth = '2px';
+          span.style.marginRight = '2px';
+          span.style.height = '20px';
+          span.style.width = '20px';
+          span.style.display = 'inline-block';
+          span.style.backgroundImage = 'url(/../assets/svgs/award.svg)';
+          span.style.backgroundRepeat = 'no-repeat';
 
-            titleLines.forEach((title: string) => {
-              const tr = document.createElement('tr');
-              tr.style.borderWidth = '0';
+          const tr = document.createElement('tr');
+          tr.style.backgroundColor = 'inherit';
+            tr.style.borderWidth = '0'
 
-              const th = document.createElement('th');
-              th.style.borderWidth = '0';
-              const text = document.createTextNode(title);
+            const td = document.createElement('td');
+            td.style.backgroundColor = 'inherit';
+            td.style.borderWidth = '0';
+            td.style.display = 'flex';
+            td.style.flexDirection = 'row';
+            td.style.alignItems = 'center';
 
-              th.appendChild(text);
-              tr.appendChild(th);
-              tableHead.appendChild(tr);
-            });
+            td.appendChild(span);
 
-            const tableBody = document.createElement('tbody');
-            bodyLines.forEach((body: string, i: string | number) => {
-              //const colors = tooltip.labelColors[i];
+            const textNode = body.at(0);
+            const text = document.createTextNode(textNode? textNode : '');
+            td.appendChild(text);
+            tr.appendChild(td);
+            tableBody.appendChild(tr);
+        });
 
-              const span = document.createElement('span');
-              //span.style.background = colors.backgroundColor;
-              //span.style.borderColor = colors.borderColor;
-              span.style.borderWidth = '2px';
-              span.style.marginRight = '2px';
-              span.style.height = '20px';
-              span.style.width = '20px';
-              span.style.display = 'inline-block';
-              span.style.backgroundImage = 'url(/../assets/svgs/award.svg)';
-              span.style.backgroundRepeat = 'no-repeat';
+        const tableRoot = tooltipEl.querySelector('table');
 
-              const tr = document.createElement('tr');
-              tr.style.backgroundColor = 'inherit';
-              tr.style.borderWidth = '0'
-
-              const td = document.createElement('td');
-              td.style.backgroundColor = 'inherit';
-              td.style.borderWidth = '0';
-              td.style.display = 'flex';
-              td.style.flexDirection = 'row';
-              td.style.alignItems = 'center';
-
-              const text = document.createTextNode(body);
-
-              td.appendChild(span);
-              td.appendChild(text);
-              tr.appendChild(td);
-              tableBody.appendChild(tr);
-            });
-
-            const tableRoot = tooltipEl.querySelector('table');
-
-            // Remove old children
-            while (tableRoot?.firstChild) {
-              tableRoot.firstChild.remove();
-            }
-
-            // Add new children
-            tableRoot?.appendChild(tableHead);
-            tableRoot?.appendChild(tableBody);
-          }
-
-          const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
-          chart.canvas.style.cursor = 'pointer';
-
-          // Display, position, and set styles for font
-          tooltipEl.style.opacity = '1';
-          tooltipEl.style.left = positionX + tooltip.caretX + 'px';
-          tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-          tooltipEl.style.font = tooltip.options.bodyFont.string;
-          tooltipEl.style.padding = tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
-        };
-
-          const _chart = new Chart(chartElement, {
-            type: 'pie',
-            data: {
-              labels: labels,
-              datasets: [{
-                label: 'Number of Medals',
-                data: medals,
-              }]
-            },
-            options: {
-              responsive: true,
-              onClick: (_event: ChartEvent, elements: ActiveElement[]) => {
-                if (elements.length > 0) {
-                  //console.log('elements', elements)
-                  const clickedElementIndex = elements[0].index;
-                  // Handle the click event for the specific slice of the pie!
-                  //console.log('Clicked slice index:', clickedElementIndex);
-                  //console.log('Clicked slice event:', event);
-                    this.router.navigateByUrl(`${clickedElementIndex + 1}`);
-                }
-              },
-              plugins: {
-                tooltip: {
-                  enabled: false,
-                  external: externalTooltipHandler,
-                  callbacks: {
-                    title: function(context) {
-                      return context[0].label;
-                    },
-                    label: function(context) {
-                      return context.formattedValue;
-                    },
-                  },
-                },
-            },
-            },
-          });
+        // Remove old children
+        while (tableRoot?.firstChild) {
+          tableRoot.firstChild.remove();
         }
+
+        // Add new children
+        tableRoot?.appendChild(tableHead);
+        tableRoot?.appendChild(tableBody);
+      }
+
+      const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
+      chart.canvas.style.cursor = 'pointer';
+
+      // Display, position, and set styles for font
+      tooltipEl.style.opacity = '1';
+      tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+      tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+      //@ts-ignore
+      //tooltipEl.style.font = tooltip.options.bodyFont.string;
+      tooltipEl.style.padding = tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
+    };
+
+    renderChart(){
+      this.chart = new Chart(this.chartElement, {
+        type: 'pie',
+        data: {
+          labels: this.labels,
+          datasets: [{
+            label: 'Number of Medals',
+            backgroundColor: this.accessibleColorPalette,
+            data: this.medals,
+          }]
+        },
+        options: {
+          maintainAspectRatio: true,
+          responsive: true,
+          onClick: (_event: ChartEvent, elements: ActiveElement[]) => {
+            if (elements.length > 0) {
+              const clickedElementIndex = elements[0].index;
+              const countryUrl = clickedElementIndex + 1;
+                this.router.navigateByUrl(`${countryUrl}`);
+            }
+          },
+          plugins: {
+            tooltip: {
+              enabled: false,
+              external: this.externalTooltipHandler,
+              callbacks: {
+                title: function(context) {
+                  return context[0].label;
+                },
+                label: function(context) {
+                  return context.formattedValue;
+                },
+              },
+            },
+        },
+        },
+      });
     }
+  }
