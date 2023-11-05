77be81583c8cd14004eb9fcca3c6e476b0a5ade3
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-11-05 18:59:16 +0100

    remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,12 @@ import { NgChartsModule } from 'ng2-charts';
 import { HomeComponent } from './pages/home/home.component';
 import { CountryComponent } from './pages/country/country.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CountryPieSliceComponent } from './components/country-pie-slice/country-pie-slice.component';
 import { HeaderComponent } from './components/header/header.component';
 import { StatsCardComponent } from './components/stats-card/stats-card.component';
 import { PiechartComponent } from './components/piechart/piechart.component';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-//import { OlympicService } from './core/services/olympic.service';
-import { routes } from './app-routing.module';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { routes } from './app-routing.module';
 
 @NgModule({
   imports: [
@@ -30,7 +28,6 @@ import { LineChartComponent } from './components/line-chart/line-chart.component
     CountryComponent,
     NotFoundComponent,
     HeaderComponent,
-    CountryPieSliceComponent,
     StatsCardComponent,
     PiechartComponent,
     LineChartComponent
