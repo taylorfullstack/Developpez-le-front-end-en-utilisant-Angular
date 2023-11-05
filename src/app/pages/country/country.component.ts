a7fdd2a86adaee6f353310c25dfa71834c2abf7d
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-11-05 19:01:24 +0100

    update country component to implent oninit and redirect when country not found

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 import { Participation } from 'src/app/core/models/Participation';
 
 @Component({
@@ -9,32 +9,33 @@ import { Participation } from 'src/app/core/models/Participation';
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent{
+export class CountryComponent implements OnInit{
+  olympics$!: Olympic[];
   country!: Olympic;
   totalMedals!: number;
   participations!: Participation[];
   athletes!: number;
+  countryId!: number;
 
-  constructor(private olympicService: OlympicService, private route: ActivatedRoute) {}
+  constructor(private olympicService: OlympicService, private route: ActivatedRoute,  private router: Router) {}
 
   ngOnInit(): void {
-    const countryId = +this.route.snapshot.params['id'];
-    this.country = this.olympicService.getOlympicById(countryId);
-    this.totalMedals = this.olympicService.getTotalMedalsByCountryId(countryId);
-    this.athletes = this.olympicService.getTotalAthletesByCountryId(countryId);
-    this.participations = this.country.participations;
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.countryId = +id; //convert string to number
+    this.getLineChartData(+id);
   }
 
-  /**
-   * Navigate to home page when clicking on the back to home button
-   *
-   * This function was added in order to try to address the issue of the pie chart not renderingafter navigating back to home from the country page.
-   *
-   * TODO: Continue to Investigate why the chart is not rendering on the home page after navigating back to it from the country page.
-   *
-   * Issue #1
-  */
-  navigateToHome() {
-    window.location.href = '';
+  getLineChartData(countryId: number): void {
+    this.olympicService.getOlympics().subscribe(olympics => {
+      this.olympics$ = olympics;
+      try {
+        this.country = this.olympicService.getOlympicById(countryId);
+        this.totalMedals = this.olympicService.getTotalMedalsByCountryId(countryId);
+        this.athletes = this.olympicService.getTotalAthletesByCountryId(countryId);
+        this.participations = this.country.participations;
+      } catch (error) {
+        this.router.navigate(['**']);
+      }
+    });
   }
 }
