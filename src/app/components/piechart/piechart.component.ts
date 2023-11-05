c7f378545cc378ae29684c8b41bf4a047140e064
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-11-05 18:39:41 +0100

    configure piechart legend, remove console.logs

--- src/app/components/piechart/piechart.component.ts
+++ src/app/components/piechart/piechart.component.ts
@@ -1,40 +1,28 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';
-import { Chart, ChartEvent, TooltipModel} from 'chart.js';
+import { Chart, ChartEvent} from 'chart.js';
 import { ActiveElement} from 'chart.js/dist/plugins/plugin.tooltip';
-import { getOrCreateTooltip, externalTooltipHandler } from 'src/app/core/utils/tooltip';
+import { externalTooltipHandler } from 'src/app/core/utils/tooltip';
 
 @Component({
   selector: 'app-piechart',
   templateUrl: './piechart.component.html',
   styleUrls: ['./piechart.component.scss']
 })
-export class PiechartComponent {
+export class PiechartComponent implements OnInit {
     @Input({ required: true }) labels!: string[];
     @Input({ required: true }) medals!: number[];
     chartElement!: HTMLCanvasElement;
     chart!: Chart<'pie'>;
     accessibleColorPalette = ["#A8385D", "#7AA3E5", "#A27EA8", "#A6E9F4", "#ADCDED", "#A95963" ]
 
-    constructor(private router: Router) {
-    }
+    constructor(private router: Router) {}
 
     ngOnInit(): void {
       const pieChartCanvas = document.getElementById('pie-chart');
       if(pieChartCanvas instanceof HTMLCanvasElement) {
         this.chartElement = pieChartCanvas;
         this.renderChart();
-        console.log('this.chartElement', this.chartElement);
-        console.log('this.chart', this.chart);
-        //console.dir(this.chart);
-        console.log('this.chart.legend', this.chart.legend);
-        console.log('this.chart.chartArea', this.chart.chartArea);
-        console.log('this.chart.legend.legendItems', this.chart.legend?.legendItems);
-        console.log('canvas aspect ratio', this.chart.aspectRatio);
-        console.log('canvas width', this.chart.canvas.getContext('2d')?.canvas.width);
-        console.log('canvas height', this.chart.canvas.getContext('2d')?.canvas.height);
-        console.log('canvas 2d conttext canvas', this.chart.canvas.getContext('2d')?.canvas);
-        //this.chart.update("resize"); //attempt to address Issue #1 - doesn't resolve
       };
     }
 
@@ -50,13 +38,11 @@ export class PiechartComponent {
           }]
         },
         options: {
-          //maintainAspectRatio: true,
-          //responsive: true,
           onClick: (_event: ChartEvent, elements: ActiveElement[]) => {
             if (elements.length > 0) {
               const clickedElementIndex = elements[0].index;
               const countryUrl = clickedElementIndex + 1;
-                this.router.navigateByUrl(`${countryUrl}`);
+                this.router.navigateByUrl(`olympic/${countryUrl}`);
             }
           },
           plugins: {
@@ -70,6 +56,20 @@ export class PiechartComponent {
                 label: function(context) {
                   return context.formattedValue;
                 },
+              },
+            },
+            legend: {
+              display: true,
+              position: 'top',
+              align: 'center',
+              labels: {
+                  usePointStyle: true,
+                  pointStyle: 'rectRounded',
+                  padding: 20,
+                  color: '#000',
+                  font: {
+                      size: 14
+                  },
               },
             },
         },
