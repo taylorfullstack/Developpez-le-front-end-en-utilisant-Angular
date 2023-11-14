2c11c97caca9bf54be4f5617f01f9bfb2b654c2a
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-11-14 21:22:07 +0100

    add comments to service

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -6,11 +6,13 @@ import { catchError} from 'rxjs/operators';
 import { Olympic } from '../models/Olympic';
 
 /**
- * @Injectable() decorator marks the class as one that participates in the dependency injection system
- * The OlympicService class is going to provide an injectable service, and it can also have its own injected dependencies.
- * It can be used to retrieve data from a web service, local storage, or a mock data source.
+ * @description
+ * The Injectable() decorator marks the class as one that participates in the dependency injection system.
  *
- * The OlympicService class is going to retrieve the data from the mock data source.
+ * The OlympicService class provides an injectable service, which can also have its own injected dependencies.
+ * An injectable service can be used to retrieve data from a web service, local storage, or a mock data source.
+ *
+ * The OlympicService class retrieves the data from the mock data source.
  */
 @Injectable({
   providedIn: 'root',
@@ -20,17 +22,17 @@ export class OlympicService {
   private olympics: Olympic[] = [];
 
   /**
-  * handleError pulled directly from the Angular docs:
-  * Handle Http operation that failed.
-  * Let the app continue.
-  *
-  * @param operation - name of the operation that failed
-  * @param result - optional value to return as the observable result
-  */
+   * @source - https://angular.io/tutorial/tour-of-heroes/toh-pt6#handleerror
+   *
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
   private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      console.log(`${operation} failed: ${error.message}`);
+    return (error: Error): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
@@ -38,6 +40,10 @@ export class OlympicService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * @method getOlympics
+   * @returns an Observable of type Olympic[]
+   */
   getOlympics(): Observable<Olympic[]> {
     return this.http.get<Olympic[]>(this.olympicUrl)
       .pipe(
@@ -48,6 +54,11 @@ export class OlympicService {
         );
     }
 
+  /**
+   * @method getOlympicById
+   * @param id - the id of the olympic country to be retrieved
+   * @returns an object of type Olympic with the specified id
+   */
   getOlympicById(id: number): Olympic {
     const olympic = this.olympics.find((olympic) => olympic.id === id);
     if(!olympic){
@@ -57,11 +68,21 @@ export class OlympicService {
     }
   }
 
+  /**
+   * @method getTotalMedalsByCountryId
+   * @param id - the id of the olympic country
+   * @returns the total number of medals won by the country
+   */
   getTotalMedalsByCountryId(id: number): number{
     const olympic = this.getOlympicById(id);
     return olympic.participations.reduce((total, participation) => total + participation.medalsCount, 0);
   };
 
+  /**
+   * @method getTotalAthletesByCountryId
+   * @param id - the id of the olympic country
+   * @returns - the total number of athletes from the country
+   */
   getTotalAthletesByCountryId(id: number): number{
     const olympic = this.getOlympicById(id);
     return olympic.participations.reduce((total, participation) => total + participation.athleteCount, 0);
